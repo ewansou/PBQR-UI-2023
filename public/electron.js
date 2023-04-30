05815cb18462291eed8991b8bcf87e6ae9defe55
ewansou <ewansou@hotmail.com>
2023-04-30 21:50:41 +0800

    update

--- public/electron.js
+++ public/electron.js
@@ -63,7 +63,7 @@ function createWindow() {
     mainWindow.loadURL(
         isDev
             ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "../build/index.html")}`
+            : `file://${__dirname}/../build/index.html`
     );
 
     mainWindow.on("closed", function () {
